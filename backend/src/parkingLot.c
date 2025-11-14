ee6107bd075ce8931505760de6f57a2b662797c1
Mikkel Buus Rasmussen <34546784+MikkelRasmussen@users.noreply.github.com>
2025-11-14 09:56:55 +0100

    Oprettede cmake filer

---
+++ backend/src/parkingLot.c
@@ -0,0 +1,5 @@
+//
+// Created by mikke on 14/11/2025.
+//
+
+#include "parkingLot.h"
---
+++ backend/src/parkingLot.h
@@ -0,0 +1,10 @@
+//
+// Created by mikke on 14/11/2025.
+//
+
+#ifndef BACKEND_PARKINGLOT_H
+#define BACKEND_PARKINGLOT_H
+
+
+
+#endif //BACKEND_PARKINGLOT_H
