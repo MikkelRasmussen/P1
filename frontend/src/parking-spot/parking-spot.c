5bee86a152bfa1632bf848a2a8ce10a534f707e3
jepp8530 <jepp8530@mail.hansenberg.dk>
2025-11-17 15:53:18 +0100

    Dette Duer For Jeppe, hvis sensitiv, så ikke Pull

--- frontend/src/parking-spot/parking-spot.c
+++ frontend/src/parking-spot/parking-spot.c
@@ -3,15 +3,18 @@
 #include <stdlib.h>
 
 void add_parking_spot(Project *project, Vector2 position, float width,
-                      float height, char zone) {
-  if (project->parking_spots_capacity == 0) {
+                      float height, char zone)
+{
+  if (project->parking_spots_capacity == 0)
+  {
     project->parking_spots_capacity = 10;
     // project->parking_spots = (ParkingSpot*)malloc(sizeof(ParkingSpot) *
     // project->parking_spots_capacity); if (!project->parking_spots) return;
   }
 
   if (project->spot_count[project->active_floor] >=
-      project->parking_spots_capacity) {
+      project->parking_spots_capacity)
+  {
     project->parking_spots_capacity *= 2;
     ParkingSpot *new_array = (ParkingSpot *)realloc(
         project->floors[project->active_floor],
