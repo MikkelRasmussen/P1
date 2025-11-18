1cef79f0a2cfdf736f6591631d4d5aa601baa70c
Mikkel Buus Rasmussen <34546784+MikkelRasmussen@users.noreply.github.com>
2025-11-18 12:13:20 +0100

    Added assignlot.c

---
+++ backend/src/assignLot.c
@@ -0,0 +1,5 @@
+//
+// Created by mikke on 18/11/2025.
+//
+
+#include "assignLot.h"
---
+++ backend/src/assignLot.h
@@ -0,0 +1,8 @@
+//
+// Created by mikke on 18/11/2025.
+//
+
+#ifndef BACKEND_ASSIGNLOT_H
+#define BACKEND_ASSIGNLOT_H
+
+#endif //BACKEND_ASSIGNLOT_H
