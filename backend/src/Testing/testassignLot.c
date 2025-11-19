636bb00310490dfabe957fb56a8a42cdda5d5c68
StrikeZ <fpbrauner@gmail.com>
2025-11-19 16:18:35 +0100

    Added testing directory and files

---
+++ backend/src/Testing/testassignLot.c
@@ -0,0 +1,5 @@
+//
+// Created by Frederik on 19-11-2025.
+//
+
+#include "testassignLot.h"
---
+++ backend/src/Testing/testassignLot.h
@@ -0,0 +1,8 @@
+//
+// Created by Frederik on 19-11-2025.
+//
+
+#ifndef P1_TESTASSIGNLOT_H
+#define P1_TESTASSIGNLOT_H
+
+#endif //P1_TESTASSIGNLOT_H
