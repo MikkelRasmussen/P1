b9c66c32fe11cd2765ab50d6f886132435de6b94
Bugge <fbj070106@gmail.com>
2025-12-15 14:36:05 +0100

    Re-enabled to more tests

--- allocator/src/assignLot.h
+++ allocator/src/assignLot.h
@@ -8,14 +8,19 @@
 #include "parkingLot.h"
 
 void assignSpace(struct parking_lot *lot, struct car *car);
-void autoAssignNewCar(struct car *newCar, struct parking_lot parkinglots[], int lotCount);
+void reAssignSpace(struct parking_lot *lot, struct car *car, int check);
+void deAssignSpace(struct parking_lot *lot, struct car *car);
+void autoAssignNewCar(struct car *newCar, struct parking_lot parkinglots[],
+                      int lotCount);
 
 int checkOccupation(struct parking_lot[], int length, struct car car);
-void parkingMenu(struct parking_lot parkinglots[], int lotCount, struct car cars[], int carCount);
+void parkingMenu(struct parking_lot parkinglots[], int lotCount,
+                 struct car cars[], int carCount);
 
 struct car *addCar(struct car *cars, int *carCount);
 struct car *removeCar(struct car *cars, int *carCount, int indexToRemove);
 
 double getZoneTime(const char *zone);
-void simulateAssignmentTime(struct car cars[], int carCount, struct parking_lot parkinglots[], int lotCount);
+void simulateAssignmentTime(struct car cars[], int carCount,
+                            struct parking_lot parkinglots[], int lotCount);
 #endif // BACKEND_ASSIGNLOT_H
--- allocator/testing/testing.c
+++ allocator/testing/testing.c
@@ -24,34 +24,34 @@ TEST_CASE(assignLot_test, {
   CHECK(lotArray[0].status == OCCUPIED, "lot number 0 should be occupied");
 })
 
-// TEST_CASE(deAssignSpace_test, {
-//   struct parking_lot lotArray[10];
-//   struct car car = {"ABC123", STANDARD, -1};
+TEST_CASE(deAssignSpace_test, {
+  struct parking_lot lotArray[10];
+  struct car car = {"ABC123", STANDARD, -1};
 
-//   for (int i = 0; i < 10; i++) {
-//     lotArray[i].floor = 1;
-//     strcpy(lotArray[i].zone, "A");
-//     lotArray[i].type = STANDARD;
-//     lotArray[i].status = OCCUPIED;
-//   }
+  for (int i = 0; i < 10; i++) {
+    lotArray[i].floor = 1;
+    strcpy(lotArray[i].zone, "A");
+    lotArray[i].type = STANDARD;
+    lotArray[i].status = OCCUPIED;
+  }
 
-//   deAssignSpace(&lotArray[0], &car);
+  deAssignSpace(&lotArray[0], &car);
 
-//   CHECK(lotArray[0].status == VACANT, "Lot status changed to vacant");
-//   CHECK(car.assigned_lot == -1,
-//         "Car is deassigned from the space and returns -1");
-// })
+  CHECK(lotArray[0].status == VACANT, "Lot status changed to vacant");
+  CHECK(car.assigned_lot == -1,
+        "Car is deassigned from the space and returns -1");
+})
 
-// TEST_CASE(reAssignSpace_test, {
-//   struct parking_lot lot = {1, "A", 0, STANDARD, VACANT};
-//   struct car car = {"ABC123", STANDARD, -1};
+TEST_CASE(reAssignSpace_test, {
+  struct parking_lot lot = {1, 1, "A", 0, STANDARD, VACANT};
+  struct car car = {"ABC123", STANDARD, -1};
 
-//   reAssignSpace(&lot, &car, 0);
+  reAssignSpace(&lot, &car, 0);
 
-//   CHECK(lot.status == ERROR,
-//         "Lot status needs to be set to Error when reassigned");
-//   CHECK(car.assigned_lot == 0, "Car assigned lots is set to 0");
-// })
+  CHECK(lot.status == ERROR,
+        "Lot status needs to be set to Error when reassigned");
+  CHECK(car.assigned_lot == 0, "Car assigned lots is set to 0");
+})
 
 TEST_CASE(checkOccupation_test, {
   struct car car = {"ABC123", STANDARD, -1};
@@ -74,5 +74,5 @@ TEST_CASE(checkOccupation_test, {
                     "they are occupied and return index 3");
 })
 
-MAIN_RUN_TESTS(assignLot_test, /*deAssignSpace_test, reAssignSpace_test,*/
+MAIN_RUN_TESTS(assignLot_test, deAssignSpace_test, reAssignSpace_test,
                checkOccupation_test)
